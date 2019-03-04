7cf3bf4129202edb2cdeccb7aad2d8f5e7bdbbf7
Maryna <marinakrasnovatr81@gmail.com>
2019-03-04 16:31:24 +0200

    no inspect

--- priv/repo/migrations/20170419082821_create_log_changes_table.exs
+++ priv/repo/migrations/20170419082821_create_log_changes_table.exs
@@ -5,8 +5,6 @@ defmodule EctoTrail.TestRepo.Migrations.CreateAuditLogTable do
   @table_name String.to_atom(Application.fetch_env!(:ecto_trail, :table_name))
 
   def change(table_name \\ @table_name) do
-    IO.inspect(table_name, label: :table_name)
-
     create table(table_name, primary_key: false) do
       add(:id, :uuid, primary_key: true)
       add(:actor_id, :string, null: false)
