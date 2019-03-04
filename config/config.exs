37f648e601f3a39e9055f54879563b6c5993a0af
Maryna <marinakrasnovatr81@gmail.com>
2019-03-04 15:46:48 +0200

    format + no encoding

--- config/config.exs
+++ config/config.exs
@@ -3,8 +3,4 @@ use Mix.Config
 config :ecto_trail,
   table_name: "audit_log"
 
-config :geo_postgis,
-  # If you want to set your JSON module
-  json_library: Poison
-
 import_config "#{Mix.env()}.exs"
